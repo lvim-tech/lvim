504eb9d4d4f2f963f4235cc221edce6e69271e37
Biser Stoilov <lvim@motion-agency.com>
2021-06-06 09:29:46 +0300

    nvim-tree style

--- lua/modules/global/configs/ui/init.lua
+++ lua/modules/global/configs/ui/init.lua
@@ -328,11 +328,17 @@ end
 function config.tree()
     vim.g.nvim_tree_disable_netrw = 0
     vim.g.nvim_tree_hide_dotfiles = 1
-    vim.g.nvim_tree_indent_markers = 1
+    vim.g.nvim_tree_indent_markers = 0
     vim.g.nvim_tree_follow = 1
     vim.g.nvim_tree_lsp_diagnostics = 1
     vim.g.nvim_tree_auto_close = true
     vim.g.nvim_tree_auto_ignore_ft = 'startify'
+    vim.g.nvim_tree_show_icons = {
+        git = 1,
+        folders = 1,
+        files = 1,
+        folder_arrows = 0
+    }
     vim.g.nvim_tree_icons = {
         default = '',
         symlink = '',
