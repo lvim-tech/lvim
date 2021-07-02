882709e2a193b0e0d14ff32aa615716a79d555ba
Biser Stoilov <lvim@motion-agency.com>
2021-07-02 22:08:05 +0300

    Revert custom configs default

--- lua/configs/custom/init.lua
+++ lua/configs/custom/init.lua
@@ -1,18 +1,16 @@
 local configs = {}
-local funcs = require "core.funcs"
 
-configs["custom_events"] = function()
-    funcs.augroups({
-        custom_bufs = {
-            {"BufWritePre", "*.go", ":Neoformat"},
-            {"BufWritePre", "*.py", ":Neoformat"},
-            {"BufWritePre", "*.rs", ":Neoformat"},
-            {"BufWritePre", "*.dart", ":Neoformat"},
-            {"BufWritePre", "*.cpp", ":Neoformat"},
-            {"BufWritePre", "*.js", ":Neoformat"},
-            {"BufWritePre", "*.ts", ":Neoformat"}
-        }
-    })
-end
+-- Remove function events from configs.global.init
+-- configs['events'] = false
+
+-- Rewrite function events from configs.global.init
+-- configs['events'] = function()
+--   your code
+-- end
+
+-- Add new function
+-- configs['my_func'] = function()
+--   your code
+-- end
 
 return configs
