b4a2bd589745de66182ff64b81d73ff03533d363
William Boman <william@redwill.se>
2021-11-02 16:01:14 +0100

    fix setting up servers via nvim-lsp-installer

--- lua/languages/global/languages/css.lua
+++ lua/languages/global/languages/css.lua
@@ -3,24 +3,15 @@
 
 local global = require("core.global")
 local languages_setup = require("languages.global.utils")
-local nvim_lsp = require("lspconfig")
 local nvim_lsp_util = require("lspconfig/util")
 local lsp_signature = require("lsp_signature")
 local default_debouce_time = 150
-local lsp_installer = require("nvim-lsp-installer")
-local lsp_installer_servers = require("nvim-lsp-installer.servers")
 
 local language_configs = {}
 
 language_configs["lsp"] = function()
-    local function start_cssls()
-        nvim_lsp.cssls.setup {
-            cmd = {
-                "node",
-                global.lsp_path ..
-                    "lsp_servers/vscode-langservers-extracted/node_modules/.bin/vscode-css-language-server",
-                "--stdio"
-            },
+    local function start_cssls(server)
+        server:setup {
             flags = {
                 debounce_text_changes = default_debouce_time
             },
@@ -37,19 +28,8 @@ language_configs["lsp"] = function()
             handlers = languages_setup.show_line_diagnostics()
         }
     end
-    local ok, cssls = lsp_installer_servers.get_server("cssls")
-    if ok then
-        if not cssls:is_installed() then
-            cssls:install()
-            lsp_installer.on_server_ready(
-                function()
-                    start_cssls()
-                end
-            )
-        else
-            start_cssls()
-        end
-    end
+
+    languages_setup.setup_lsp("cssls", start_cssls)
 end
 
 return language_configs
