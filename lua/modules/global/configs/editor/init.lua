442164def15a93204ca7d6dbb2c91587dffb1d16
Biser Stoilov <lvim@motion-agency.com>
2021-11-12 07:42:46 +0200

    Add <gg> and <G> to Neoscroll

--- lua/modules/global/configs/editor/init.lua
+++ lua/modules/global/configs/editor/init.lua
@@ -256,6 +256,8 @@ function config.neoscroll()
                 "<C-d>",
                 "<C-b>",
                 "<C-f>",
+                "gg",
+                "G",
                 "zt",
                 "zz",
                 "zb"
