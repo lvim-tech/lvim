00b198921e8c0b6745eb8bce018cea2b7ea52c8c
Biser Stoilov <lvim@motion-agency.com>
2021-11-12 17:48:47 +0200

    Add BufReadPost to GitSigns

--- lua/modules/custom/configs/editor/init.lua
+++ lua/modules/custom/configs/editor/init.lua
@@ -1,3 +1,29 @@
 local config = {}
 
+function config.package_info()
+    require("package-info").setup(
+        {
+            icons = {
+                enable = true,
+                style = {
+                    up_to_date = "|  ",
+                    outdated = "|  "
+                }
+            },
+            autostart = true
+        }
+    )
+end
+
+function config.lvim_dependencies()
+    require("lvim-dependencies").setup({
+        jsts = {
+            az = {
+                nie = {"iznenadaaaaaaaa"}
+            }
+        }
+    })
+end
+
+
 return config
