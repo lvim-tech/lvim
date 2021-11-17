e89aa81bf5276d15e61df5f4a884233418d4db06
Biser Stoilov <lvim@motion-agency.com>
2021-11-17 13:18:47 +0200

    Prevent start angularls for other ts

--- lua/languages/global/languages/angular.lua
+++ lua/languages/global/languages/angular.lua
@@ -28,10 +28,27 @@ language_configs["lsp"] = function()
             root_dir = nvim_lsp_util.root_pattern("angular.json")
         }
     end
-    local project_root_path = vim.fn.getcwd()
-    if funcs.file_exists(project_root_path .. "/angular.json") then
-        languages_setup.setup_lsp("angularls", start_angularls)
+    languages_setup.setup_lsp("angularls", start_angularls)
+    local function start_tsserver(server)
+        server:setup {
+            flags = {
+                debounce_text_changes = default_debouce_time
+            },
+            autostart = true,
+            filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+            on_attach = function(client, bufnr)
+                table.insert(global["languages"]["jsts"]["pid"], client.rpc.pid)
+                vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+                lsp_signature.on_attach(languages_setup.config_lsp_signature)
+                languages_setup.document_highlight(client)
+            end,
+            capabilities = languages_setup.get_capabilities(),
+            root_dir = function(fname)
+                return nvim_lsp_util.find_git_ancestor(fname) or vim.fn.getcwd()
+            end
+        }
     end
+    languages_setup.setup_lsp("tsserver", start_tsserver)
 end
 
 return language_configs
