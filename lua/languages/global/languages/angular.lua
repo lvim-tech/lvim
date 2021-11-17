f0859049f4e86ec5042222d8a07459f7f2f2401c
Biser Stoilov <lvim@motion-agency.com>
2021-11-17 13:54:26 +0200

    LVIM IDE v1.1.1

--- lua/languages/global/languages/angular.lua
+++ lua/languages/global/languages/angular.lua
@@ -2,7 +2,6 @@
 -- :LspInstall angularls
 
 local global = require("core.global")
-local funcs = require("core.funcs")
 local languages_setup = require("languages.global.utils")
 local nvim_lsp_util = require("lspconfig/util")
 local lsp_signature = require("lsp_signature")
@@ -17,7 +16,7 @@ language_configs["lsp"] = function()
                 debounce_text_changes = default_debouce_time
             },
             autostart = true,
-            filetypes = {"typescript", "html", "typescriptreact", "typescript.tsx"},
+            filetypes = {"typescript"},
             on_attach = function(client, bufnr)
                 table.insert(global["languages"]["angular"]["pid"], client.rpc.pid)
                 vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -25,7 +24,9 @@ language_configs["lsp"] = function()
                 languages_setup.document_highlight(client)
             end,
             capabilities = languages_setup.get_capabilities(),
-            root_dir = nvim_lsp_util.root_pattern("angular.json")
+            root_dir = function(fname)
+                return nvim_lsp_util.find_git_ancestor(fname) or vim.fn.getcwd()
+            end
         }
     end
     languages_setup.setup_lsp("angularls", start_angularls)
