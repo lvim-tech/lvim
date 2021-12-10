9cbb56151fffffeb77037ae09b737cd7a969bcf3
Biser Stoilov <lvim@motion-agency.com>
2021-12-10 15:06:32 +0200

    Change nvim-tree setup

--- lua/modules/global/configs/ui/init.lua
+++ lua/modules/global/configs/ui/init.lua
@@ -74,40 +74,21 @@ function config.nvim_tree()
         }
     }
     require("nvim-tree").setup {
-        disable_netrw = true,
-        hijack_netrw = true,
-        open_on_setup = false,
-        ignore_ft_on_setup = {},
-        auto_close = false,
-        open_on_tab = false,
-        hijack_cursor = false,
         update_cwd = true,
-        diagnostics = {
+        update_to_buf_dir = {
             enable = true,
-            icons = {
-                error = "",
-                warning = "",
-                hint = "",
-                info = ""
-            }
+            auto_open = true
         },
         update_focused_file = {
-            enable = true,
-            update_cwd = false,
-            ignore_list = {}
+            enable = true
         },
-        system_open = {
-            cmd = nil,
-            args = {}
+        git = {
+            enable = true,
+            ignore = true,
+            timeout = 5000
         },
         view = {
-            width = 30,
-            side = "left",
-            auto_resize = true,
-            mappings = {
-                custom_only = false,
-                list = {}
-            }
+            auto_resize = true
         }
     }
 end
