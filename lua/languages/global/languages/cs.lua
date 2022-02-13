28eb61b6f8d4196ca1723f3e696e1f301d5f4291
Biser Stoilov <lvim@motion-agency.com>
2022-02-13 14:07:27 +0200

    C# DAP

--- lua/languages/global/languages/cs.lua
+++ lua/languages/global/languages/cs.lua
@@ -2,10 +2,13 @@
 -- :LspInstall omnisharp
 
 local global = require("core.global")
+local funcs = require("core.funcs")
 local languages_setup = require("languages.global.utils")
 local nvim_lsp_util = require("lspconfig/util")
 local lsp_signature = require("lsp_signature")
 local default_debouce_time = 150
+local dap_install = require("dap-install")
+local dap = require("dap")
 
 local language_configs = {}
 
@@ -30,6 +33,26 @@ language_configs["lsp"] = function()
         }
     end
     languages_setup.setup_lsp("omnisharp", start_omnisharp)
+end
+
+language_configs["dap"] = function()
+    if funcs.dir_exists(global.lsp_path .. "dapinstall/dnetcs/") ~= true then
+        vim.cmd("DIInstall dnetcs")
+    end
+    dap_install.config("dnetcs", {})
+    dap.configurations.cs = {
+        {
+            type = "netcoredbg",
+            request = "launch",
+            name = "Launch",
+            program = function()
+                return vim.fn.input(
+                    global.lsp_path .. "dapinstall/dnetcs/netcoredbg/ManagedPart.dll",
+                    vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                )
+            end
+        }
+    }
 end
 
 return language_configs
