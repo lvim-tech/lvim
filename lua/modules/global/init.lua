1bb48b831079b9f233a6c193450281178a7ac6b4
Biser Stoilov <lvim@motion-agency.com>
2022-03-08 16:30:31 +0200

    Plugins fix

--- lua/modules/global/init.lua
+++ lua/modules/global/init.lua
@@ -252,24 +252,6 @@ modules["anuvyklack/pretty-fold.nvim"] = {
     config = editor_config.pretty_fold
 }
 
-modules["fedepujol/move.nvim"] = {
-    branch = "unicode",
-    event = {
-        "BufRead",
-        "BufReadPre"
-    },
-    config = function()
-        vim.api.nvim_set_keymap('n', '<A-j>', ":MoveLine(1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<A-k>', ":MoveLine(-1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('v', '<A-j>', ":MoveBlock(1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('v', '<A-k>', ":MoveBlock(-1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<A-l>', ":MoveHChar(1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<A-h>', ":MoveHChar(-1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('v', '<A-l>', ":MoveHBlock(1)<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('v', '<A-h>', ":MoveHBlock(-1)<CR>", { noremap = true, silent = true })
-    end
-}
-
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 -- Version control ----------------------------------------------
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
