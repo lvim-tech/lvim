b8921bc600ae62d2cb0cfd9c17e9d956b241998c
Biser Stoilov <lvim@motion-agency.com>
2022-06-05 09:16:40 +0300

    WinBar customization

--- lua/core/ui/winbar.lua
+++ lua/core/ui/winbar.lua
@@ -1,9 +1,30 @@
 local M = {}
 
-local status_gps_ok, gps = pcall(require, "nvim-gps")
-if not status_gps_ok then
-    return
-end
+M.winbar_filetype_exclude = {
+    "^git.*",
+    "ctrlspace",
+    "ctrlspace_help",
+    "packer",
+    "undotree",
+    "diff",
+    "Outline",
+    "NvimTree",
+    "LvimHelper",
+    "floaterm",
+    "Trouble",
+    "dashboard",
+    "vista",
+    "spectre_panel",
+    "DiffviewFiles",
+    "flutterToolsOutline",
+    "log",
+    "qf",
+    "dapui_scopes",
+    "dapui_breakpoints",
+    "dapui_stacks",
+    "dapui_watches",
+    "calendar",
+}
 
 local function isempty(s)
     return s == nil or s == ""
@@ -13,30 +34,20 @@ local colors = _G.LVIM_COLORS()
 local hl_group_1 = "FileTextColor"
 vim.api.nvim_set_hl(0, hl_group_1, { fg = colors.green, bg = colors.status_line_bg, bold = true })
 
-M.filename = function()
+local get_filename = function()
     local filename = vim.fn.expand("%:t")
-    local extension = ""
-    local file_icon = ""
-    local file_icon_color = ""
-    local default_file_icon = ""
-    local default_file_icon_color = ""
+    local extension = vim.fn.expand("%:e")
     if not isempty(filename) then
-        extension = vim.fn.expand("%:e")
-        local default = false
-        if isempty(extension) then
-            extension = ""
-            default = true
-        end
-        file_icon, file_icon_color = require("nvim-web-devicons").get_icon_color(
+        local file_icon, file_icon_color = require("nvim-web-devicons").get_icon_color(
             filename,
             extension,
-            { default = default }
+            { default = true }
         )
         local hl_group_2 = "FileIconColor" .. extension
         vim.api.nvim_set_hl(0, hl_group_2, { fg = file_icon_color, bg = colors.status_line_bg })
-        if file_icon == nil then
-            file_icon = default_file_icon
-            file_icon_color = default_file_icon_color
+        if isempty(file_icon) then
+            file_icon = ""
+            file_icon_color = ""
         end
         return " "
             .. "%#"
@@ -53,24 +64,48 @@ M.filename = function()
     end
 end
 
-M.gps_treesitter = function()
-    local retval = M.filename()
+local get_gps = function()
+    local status_gps_ok, gps = pcall(require, "nvim-gps")
+    if not status_gps_ok then
+        return ""
+    end
+
     local status_ok, gps_location = pcall(gps.get_location, {})
     if not status_ok then
-        return retval
+        return ""
     end
-    if not gps.is_available() then
-        return retval
+
+    if not gps.is_available() or gps_location == "error" then
+        return ""
     end
-    if gps_location == "error" then
-        return retval
+
+    if not isempty(gps_location) then
+        return " ➤ " .. " " .. gps_location:gsub(" > ", " ➤ ")
     else
-        if not isempty(gps_location) then
-            local location = gps_location:gsub(" > ", " ➤ ")
-            return retval .. "%#" .. hl_group_1 .. "#" .. " ➤ " .. "%*" .. location
-        else
-            return retval
-        end
+        return ""
+    end
+end
+
+local excludes = function()
+    if vim.tbl_contains(M.winbar_filetype_exclude, vim.bo.filetype) then
+        vim.opt_local.winbar = nil
+        return true
+    end
+    return false
+end
+
+M.get_winbar = function()
+    if excludes() then
+        return
+    end
+    local value = get_filename()
+    if not isempty(value) then
+        local gps_value = get_gps()
+        value = value .. " " .. gps_value
+    end
+    local status_ok, _ = pcall(vim.api.nvim_set_option_value, "winbar", value, { scope = "local" })
+    if not status_ok then
+        return
     end
 end
 
