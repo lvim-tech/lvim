dbf91ae8cabdfdc2f467c53232b048d921f0c56e
Biser Stoilov <lvim@motion-agency.com>
2022-06-17 08:11:40 +0300

    Reformat API

--- lua/modules/user/init.lua
+++ lua/modules/user/init.lua
@@ -1,268 +1,80 @@
 local modules = {}
 
-modules["wakatime/vim-wakatime"] = {}
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- HELP ---------------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-modules["christoomey/vim-tmux-navigator"] = {}
+-- Disable of default Module (Plug-in) (from lua/modules/base/init.lua)
 
-modules["lvim-tech/lvim-kbrd"] = {
-    event = {
-        "BufRead",
-    },
-    config = function()
-        require("lvim-kbrd").setup()
-        local map = require("lvim-kbrd.utils").map
-        map("n", "<C-c>l", ":LvimKbrdToggle<CR>", { silent = false })
-    end,
-}
+-- You can disable of any default Module (Plug-in)
+-- modules["glepnir/dashboard-nvim"] = false
 
-modules["tjdevries/nlua.nvim"] = {
-    requires = "neovim/nvim-lspconfig",
-    event = {
-        "BufRead",
-    },
-    config = function()
-        local languages_setup = require("languages.base.utils")
-        require("nlua.lsp.nvim").setup(require("lspconfig"), {
-            on_attach = function(client, bufnr)
-                vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-                languages_setup.document_highlight(client, bufnr)
-                languages_setup.document_formatting(client, bufnr)
-            end,
-        })
-    end,
-}
+-- Rewrite of settings of default Module (Plug-in) (from lua/modules/base/init.lua)
 
-modules["smjonas/inc-rename.nvim"] = {
-    cmd = "IncRename",
-    config = function()
-        if vim.fn.has("nvim-0.8") == 1 then
-            require("inc_rename").setup()
-        end
-    end,
-}
-
-modules["renerocksai/calendar-vim"] = {
-    cmd = { "Calendar", "CalendarH", "CalendarT", "CalendarVR" },
-    config = function()
-        vim.g.calendar_diary_extension = ".org"
-        vim.g.calendar_diary = "~/BS/diary/"
-        vim.g.calendar_diary_path_pattern = "{YYYY}-{MM}-{DD}{EXT}"
-        vim.g.calendar_monday = 1
-        vim.g.calendar_weeknm = 1
-    end,
-}
-
-modules["nvim-orgmode/orgmode"] = {
-    ft = "org",
-    config = function()
-        require("orgmode").setup_ts_grammar()
-        require("nvim-treesitter.configs").setup({
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { "org" },
-            },
-        })
-        require("orgmode").setup({
-            org_agenda_files = { "~/BS/*" },
-            org_default_notes_file = "~/BS/reindex.org",
-        })
-    end,
-}
-
-modules["lvim-tech/org-bullets.nvim"] = {
-    branch = "lvim",
-    ft = "org",
-    config = function()
-        require("org-bullets").setup()
-    end,
-}
-
-modules["lvim-tech/lvim-orglinks"] = {
-    config = function()
-        require("lvim-orglinks").setup()
-    end,
-}
-
--- modules["lvim-tech/lvim-projects"] = {
---     rocks = "lunajson",
---     config = function()
---         -- require("lvim-projects").setup()
---         -- local map = require("lvim-projects.utils").map
---         -- map("n", "<C-c>l", ":LvimKbrdToggle<CR>", { silent = false })
---     end,
+-- You can rewrite of settings of any of default Module (Plug-in)
+-- modules["glepnir/dashboard-nvim"] = {
+--     -- your code
 -- }
 
--- modules["NTBBloodbath/rest.nvim"] = {
---     requires = { "nvim-lua/plenary.nvim" },
---     config = function()
---         require("rest-nvim").setup({})
---     end,
+-- Add new Module (Plug-in)
+
+-- You can add new Module (Plug-in)
+-- modules["sheerun/vim-polyglot"] = {
+--     your code
 -- }
 
---[[ modules["smjonas/snippet-converter.nvim"] = {
-    -- SnippetConverter uses semantic versioning. Example: use version = "1.*" to avoid breaking changes on version 1.
-    -- Uncomment the next line to follow stable releases only.
-    -- version = "*",
-    config = function()
-        local template = {
-            -- name = "t1", (optionally give your template a name to refer to it in the `ConvertSnippets` command)
-            sources = {
-                ultisnips = {
-                    -- Add snippets from (plugin) folders or individual files on your runtimepath...
-                    "/home/biserstoilov/.config/nvim/vim-snippets/UltiSnips",
-                    -- ...or use absolute paths on your system.
-                },
-                snipmate = {
-                    "/home/biserstoilov/.config/nvim/vim-snippets/snippets",
-                },
-            },
-            output = {
-                -- Specify the output formats and paths
-                vscode_luasnip = {
-                    "/home/biserstoilov/.config/nvim/luasnip_snippets",
-                },
-            },
-        }
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- HELP ---------------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-        require("snippet_converter").setup({
-            templates = { template },
-            -- To change the default settings (see configuration section in the documentation)
-            -- settings = {},
-        })
-    end,
-} ]]
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- UI -----------------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
--- modules["tversteeg/registers.nvim"] = {}
+-- local ui_config = require("modules.user.configs.ui")
 
---[[ modules["vimwiki/vimwiki"] = {
-    branch = "dev",
-    config = function()
-        local home = vim.fn.expand("~/ORG")
-        vim.g.vimwiki_list = {
-            {
-                path = home .. "/",
-                template_path = home .. "/" .. "templates/",
-                -- template_default = "default",
-                syntax = "org",
-                ext = ".org",
-                -- path_html = home .. "/" .. "site_html/",
-                -- custom_wiki2html = "vimwiki_markdown",
-                -- template_ext = ".tpl",
-            },
-        }
-        local colors = _G.LVIM_COLORS()
-        local hl_group = "calendar"
-        vim.api.nvim_set_hl(0, hl_group, { bg = colors.status_line_bg })
-    end,
-} ]]
+-- modules["name_of_your/module"] = {
+--     config = ui_config.name_of_your_function
+-- }
 
---[[ modules["lukas-reineke/headlines.nvim"] = {
-    config = function()
-        require("headlines").setup({
-            markdown = {
-                source_pattern_start = "^```",
-                source_pattern_end = "^```$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^#+",
-                headline_highlights = { "Headline" },
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash",
-                dash_string = "-",
-                fat_headlines = true,
-            },
-            rmd = {
-                source_pattern_start = "^```",
-                source_pattern_end = "^```$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^#+",
-                headline_signs = { "Headline" },
-                codeblock_sign = "CodeBlock",
-                dash_highlight = "Dash",
-                dash_string = "-",
-                fat_headlines = true,
-            },
-            vimwiki = {
-                source_pattern_start = "^{{{%a+",
-                source_pattern_end = "^}}}$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^=+",
-                headline_highlights = { "Headline" },
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash",
-                dash_string = "-",
-                fat_headlines = true,
-            },
-            org = {
-                source_pattern_start = "#%+[bB][eE][gG][iI][nN]_[sS][rR][cC]",
-                source_pattern_end = "#%+[eE][nN][dD]_[sS][rR][cC]",
-                dash_pattern = "^-----+$",
-                headline_pattern = "^%*+",
-                headline_highlights = { "Headline" },
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash",
-                dash_string = "-",
-                fat_headlines = true,
-            },
-        })
-    end,
-} ]]
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- Editor -------------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
---[[ modules["ranjithshegde/orgWiki.nvim"] = {
-    ft = "org",
-    config = function()
-        require("orgWiki").setup({
-            wiki_path = { "~/BS/" },
-            diary_path = "~/BS/diary/",
-        })
-    end,
-} ]]
+-- local editor_config = require("modules.user.configs.editor")
 
--- modules["renerocksai/telekasten.nvim"] = {
---     requires = {
---         "renerocksai/calendar-vim",
---         after = "telekasten.nvim",
---     },
---     config = function()
---         local home = vim.fn.expand("~/Notes")
---         require("telekasten").setup({
---             home = home,
---             take_over_my_home = true,
---             auto_set_filetype = true,
---             dailies = home .. "/" .. "daily",
---             weeklies = home .. "/" .. "weekly",
---             templates = home .. "/" .. "templates",
---             image_subdir = "img",
---             extension = ".md",
---             new_note_filename = "title",
---             uuid_type = "%Y%m%d%H%M",
---             uuid_sep = "-",
---             follow_creates_nonexisting = true,
---             dailies_create_nonexisting = true,
---             weeklies_create_nonexisting = true,
---             journal_auto_open = false,
---             template_new_note = home .. "/" .. "templates/new_note.md",
---             template_new_daily = home .. "/" .. "templates/daily.md",
---             template_new_weekly = home .. "/" .. "templates/weekly.md",
---             image_link_style = "markdown",
---             sort = "filename",
---             plug_into_calendar = true,
---             calendar_opts = {
---                 weeknm = 4,
---                 calendar_monday = 1,
---                 calendar_mark = "left-fit",
---             },
---             close_after_yanking = false,
---             insert_after_inserting = true,
---             tag_notation = "#tag",
---             command_palette_theme = "ivy",
---             show_tags_theme = "ivy",
---             subdirs_in_links = true,
---             template_handling = "smart",
---             new_note_location = "smart",
---             rename_update_links = true,
---         })
---         vim.api.nvim_set_keymap("n", "tc", ":Telekasten show_calendar<CR>", { silent = true })
---     end,
+-- modules["name_of_your/module"] = {
+--     config = editor_config.name_of_your_function
+-- }
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- Version control ----------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+-- local version_control_config = require("modules.user.configs.version_control")
+
+-- modules["name_of_your/module"] = {
+--     config = version_control_config.name_of_your_function
+-- }
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- Languages ----------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+-- local languages_config = require("modules.user.configs.editor")
+
+-- modules["name_of_your/module"] = {
+--     config = languages_config.name_of_your_function
+-- }
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+-- Completion ---------------------------------------------------
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+-- local completion_config = require("modules.user.configs.editor")
+
+-- modules["name_of_your/module"] = {
+--     config = completion_config.name_of_your_function
 -- }
 
 return modules
