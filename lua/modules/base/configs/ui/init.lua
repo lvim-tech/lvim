1f0aed61c81c40ddf7adf73756bd49931dc59520
Biser Stoilov <lvim@motion-agency.com>
2022-07-17 07:52:47 +0300

    Add Trouble and ctrlspace to lvim_sidebars

--- lua/modules/base/configs/ui/init.lua
+++ lua/modules/base/configs/ui/init.lua
@@ -1,7 +1,7 @@
 local config = {}
 
 function config.lvim_colorscheme()
-    vim.g.lvim_sidebars = { "qf", "Outline", "terminal", "packer", "calendar", "spectre_panel" }
+    vim.g.lvim_sidebars = { "qf", "Outline", "terminal", "packer", "calendar", "spectre_panel", "Trouble", "ctrlspace" }
     vim.cmd("colorscheme lvim")
 end
 
@@ -702,7 +702,7 @@ function config.heirline_nvim()
             local extension = vim.fn.expand("%:e")
             if not isempty(filename) then
                 local file_icon, file_icon_color =
-                    require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+                require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
                 local hl_group_2 = "FileIconColor" .. extension
                 vim.api.nvim_set_hl(0, hl_group_2, { fg = file_icon_color, bg = colors.status_line_bg })
                 if isempty(file_icon) then
