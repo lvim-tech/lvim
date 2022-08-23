527934a8aee9bf7355e17ce98035629a8d7bd5d7
Biser Stoilov <lvim@motion-agency.com>
2022-08-23 20:59:14 +0300

    Cmp fix

--- lua/modules/base/configs/completion/init.lua
+++ lua/modules/base/configs/completion/init.lua
@@ -11,8 +11,8 @@ function config.nvim_cmp()
     end
     require("luasnip.loaders.from_vscode").lazy_load()
     local check_backspace = function()
-        local col = vim.fn.col(".") - 1
-        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
     local lsp_symbols = {
         Text = "   (Text) ",
