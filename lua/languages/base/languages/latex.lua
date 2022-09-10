024d25fa26141bbdead2c235073303fabdf84c95
Biser Stoilov <lvim@motion-agency.com>
2022-09-10 21:35:20 +0300

    Tex lsp - relove ltex, add texlab

--- lua/languages/base/languages/latex.lua
+++ lua/languages/base/languages/latex.lua
@@ -1,12 +1,12 @@
 local languages_setup = require("languages.base.utils")
-local ltex_config = require("languages.base.languages._configs").default_config({ "bib", "tex" }, "latex")
+local texlab_config = require("languages.base.languages._configs").default_config({ "bib", "tex" }, "latex")
 
 local language_configs = {}
 
 language_configs["lsp"] = function()
     languages_setup.setup_languages({
         ["language"] = "latex",
-        ["ltex-ls"] = { "ltex", ltex_config },
+        ["texlab"] = { "texlab", texlab_config },
     })
 end
 
