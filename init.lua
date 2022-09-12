42e726e202ace34699dd63de96967fcfbdb81ed5
Biser Stoilov <lvim@motion-agency.com>
2022-09-12 19:36:55 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 pcall(require, "impatient")
--- vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "10.15")
+-- For osx bigsur bug
+-- Issue: [Luarocks fails to install on macOS BigSur #180](https://github.com/wbthomason/packer.nvim/issues/180)
+-- if vim.fn.has("mac") == 1 then
+--     vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "10.15")
+-- end
 if vim.fn.has("nvim-0.7") == 1 then
     require("core")
 else
