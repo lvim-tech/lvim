927c50a1d2627d99f9c6a46fd6b6fab7b0e17a35
Biser Stoilov <lvim@motion-agency.com>
2022-10-07 17:32:40 +0300

    Add variables

---
+++ lua/configs/base/ui/colors.lua
@@ -0,0 +1,13 @@
+local funcs = require("core.funcs")
+
+return {
+    color_01 = funcs.get_highlight("CursorLineNr").fg,
+    color_02 = funcs.get_highlight("DiagnosticError").fg,
+    color_03 = funcs.get_highlight("DiagnosticWarn").fg,
+    color_04 = funcs.get_highlight("DiagnosticHint").fg,
+    color_05 = funcs.get_highlight("DiagnosticInfo").fg,
+    status_line_bg = funcs.get_highlight("StatusLine").bg,
+    status_line_fg = funcs.get_highlight("StatusLine").fg,
+    status_line_nc_bg = funcs.get_highlight("StatusLineNC").bg,
+    status_line_nc_fg = funcs.get_highlight("StatusLineNC").fg,
+}
