321766fdf10347f5dbaeeb9ff9a462e632ad5f7b
Biser Stoilov <lvim@motion-agency.com>
2022-10-09 10:22:34 +0300

    Elixir LSP fix, add debug

--- lua/languages/base/languages/elixir.lua
+++ lua/languages/base/languages/elixir.lua
@@ -1,13 +1,39 @@
+local global = require("core.global")
 local languages_setup = require("languages.base.utils")
-local elixirls_config = require("languages.base.languages._configs").default_config({ "elixir", "eelixir" }, "elixir")
+local elixirls_config =
+    require("languages.base.languages._configs").elixir_config_config({ "elixir", "eelixir" }, "elixir")
+local dap = require("dap")
 
 local language_configs = {}
 
 language_configs["lsp"] = function()
     languages_setup.setup_languages({
         ["language"] = "elixir",
-        ["elixir-lsp"] = { "elixirls", elixirls_config },
+        ["elixir-ls"] = { "elixir-ls", elixirls_config },
     })
+end
+
+language_configs["dap"] = function()
+    dap.adapters.mix_task = {
+        type = "executable",
+        command = global.mason_path .. "/bin/elixir-ls-debugger", -- debugger.bat for windows
+        args = {},
+    }
+    dap.configurations.elixir = {
+        {
+            type = "mix_task",
+            name = "mix test",
+            task = "test",
+            taskArgs = { "--trace" },
+            request = "launch",
+            startApps = true, -- for Phoenix projects
+            projectDir = "${workspaceFolder}",
+            requireFiles = {
+                "test/**/test_helper.exs",
+                "test/**/*_test.exs",
+            },
+        },
+    }
 end
 
 return language_configs
