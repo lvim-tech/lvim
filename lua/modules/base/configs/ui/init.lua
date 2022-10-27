8fb8da733de1a6c30562756ed524dd1a62b6193b
Biser Stoilov <lvim@motion-agency.com>
2022-10-27 20:57:34 +0300

    vim.ui.input and vim.ui.select fix

--- lua/modules/base/configs/ui/init.lua
+++ lua/modules/base/configs/ui/init.lua
@@ -19,8 +19,161 @@ function config.lvim_colorscheme()
 end
 
 function config.nui_nvim()
-    vim.ui.input = require("configs.base.ui.input")
-    vim.ui.select = require("configs.base.ui.select")
+    local function get_prompt_text(prompt, default_prompt)
+        local prompt_text = prompt or default_prompt
+        if prompt_text:sub(-1) == ":" then
+            prompt_text = " " .. prompt_text:sub(1, -2) .. " "
+        end
+        return prompt_text
+    end
+    local Input = require("nui.input")
+    local Menu = require("nui.menu")
+    local event = require("nui.utils.autocmd").event
+    local function override_ui_input()
+        local UIInput = Input:extend("UIInput")
+        function UIInput:init(opts, on_done)
+            local border_top_text = get_prompt_text(opts.prompt, "[Input]")
+            local default_value = tostring(opts.default)
+            UIInput.super.init(self, {
+                relative = "cursor",
+                position = {
+                    row = 1,
+                    col = 0,
+                },
+                size = {
+                    width = math.max(40, vim.api.nvim_strwidth(default_value) + 40),
+                },
+                border = {
+                    highlight = "NuiBorder",
+                    style = { " ", " ", " ", " ", " ", " ", " ", " " },
+                    text = {
+                        top = border_top_text,
+                        top_align = "center",
+                    },
+                },
+                win_options = {
+                    winhighlight = "Normal:NuiBody",
+                },
+            }, {
+                default_value = default_value,
+                on_close = function()
+                    on_done(nil)
+                end,
+                on_submit = function(value)
+                    on_done(value)
+                end,
+            })
+            self:on(event.BufLeave, function()
+                on_done(nil)
+            end, { once = true })
+            self:map("n", "<Esc>", function()
+                on_done(nil)
+            end, { noremap = true, nowait = true })
+        end
+        local input_ui
+        vim.ui.input = function(opts, on_confirm)
+            assert(type(on_confirm) == "function", "missing on_confirm function")
+            if input_ui then
+                vim.api.nvim_err_writeln("busy: another input is pending!")
+                return
+            end
+            input_ui = UIInput(opts, function(value)
+                if input_ui then
+                    input_ui:unmount()
+                end
+                on_confirm(value)
+                input_ui = nil
+            end)
+            input_ui:mount()
+        end
+    end
+    local function override_ui_select()
+        local UISelect = Menu:extend("UISelect")
+        function UISelect:init(items, opts, on_done)
+            local border_top_text = get_prompt_text(opts.prompt, "Select Item")
+            local kind = opts.kind or "unknown"
+            local format_item = opts.format_item
+                or function(item)
+                    return tostring(item.__raw_item or item)
+                end
+            local popup_options = {
+                relative = "editor",
+                position = "50%",
+                border = {
+                    highlight = "NuiBorder",
+                    style = { " ", " ", " ", " ", " ", " ", " ", " " },
+                    text = {
+                        top = border_top_text,
+                        top_align = "center",
+                    },
+                },
+                win_options = {
+                    winhighlight = "Normal:NuiBody",
+                },
+                zindex = 999,
+            }
+            if kind == "codeaction" then
+                popup_options.relative = "cursor"
+                popup_options.position = {
+                    row = 1,
+                    col = 0,
+                }
+            end
+            local max_width = popup_options.relative == "editor" and vim.o.columns - 4
+                or vim.api.nvim_win_get_width(0) - 4
+            local max_height = popup_options.relative == "editor" and math.floor(vim.o.lines * 80 / 100)
+                or vim.api.nvim_win_get_height(0)
+            local menu_items = {
+                UISelect.separator("", {
+                    char = " ",
+                }),
+            }
+            for index, item in ipairs(items) do
+                if type(item) ~= "table" then
+                    item = { __raw_item = item }
+                end
+                item.index = index
+                local item_text = string.sub(format_item(item), 0, max_width)
+                table.insert(menu_items, Menu.item(item_text, item))
+
+                -- menu_items[index] = Menu.item(item_text, item)
+            end
+            local menu_options = {
+                min_width = vim.api.nvim_strwidth(border_top_text),
+                max_width = max_width,
+                max_height = max_height,
+                lines = menu_items,
+                on_close = function()
+                    on_done(nil, nil)
+                end,
+                on_submit = function(item)
+                    on_done(item.__raw_item or item, item.index)
+                end,
+            }
+            UISelect.super.init(self, popup_options, menu_options)
+            self:on(event.BufLeave, function()
+                on_done(nil, nil)
+            end, { once = true })
+        end
+        local select_ui = nil
+        vim.ui.select = function(items, opts, on_choice)
+            assert(type(on_choice) == "function", "missing on_choice function")
+            if select_ui then
+                vim.api.nvim_err_writeln("busy: another select is pending!")
+                return
+            end
+            select_ui = UISelect(items, opts, function(item, index)
+                if select_ui then
+                    select_ui:unmount()
+                end
+                on_choice(item, index)
+                select_ui = nil
+            end)
+            select_ui:mount()
+        end
+    end
+    override_ui_input()
+    override_ui_select()
 end
 
 function config.nvim_notify()
