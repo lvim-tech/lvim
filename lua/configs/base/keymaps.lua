34549df5218138ca160a7d66f6f933757c53e211
Biser Stoilov <lvim@motion-agency.com>
2022-10-30 16:00:13 +0200

    Dap - move keymaps to plugin config

--- lua/configs/base/keymaps.lua
+++ lua/configs/base/keymaps.lua
@@ -42,16 +42,6 @@ keymaps["normal"] = {
     { "dc", ":LspShowDiagnosticCurrent<CR>" }, -- Lsp show diagnostic current line
     { "dn", ":LspShowDiagnosticNext<CR>" }, -- Lsp show diagnostic next line
     { "dp", ":LspShowDiagnostigPrev<CR>" }, -- Lsp show diagnostic prev line
-    { "<A-1>", "<Cmd>DapToggleBreakpoint<CR>" }, -- Toggle breakpoint
-    { "<A-2>", "<Cmd>DapStartContinue<CR>" }, -- Start / continue
-    { "<A-3>", "<Cmd>DapStepInto<CR>" }, -- Step into
-    { "<A-4>", "<Cmd>DapStepOver<CR>" }, -- Step over
-    { "<A-5>", "<Cmd>DapStepOut<CR>" }, -- Step out
-    { "<A-6>", "<Cmd>DapUp<CR>" }, -- Up
-    { "<A-7>", "<Cmd>DapDown<CR>" }, -- Down
-    { "<A-8>", "<Cmd>DapUIClose<CR>" }, -- UI close
-    { "<A-9>", "<Cmd>DapRestart<CR>" }, -- Restart
-    { "<A-0>", "<Cmd>DapToggleRepl<CR>" }, -- Toggle Repl
     { "zp", "<Cmd>FoldPreview<CR>" }, -- Fix indent + fold lines
     { "zo", "zo:IndentBlanklineRefresh<CR>" }, -- Fix indent + fold lines
     { "zO", "zO:IndentBlanklineRefresh<CR>" }, -- Fix indent + fold lines
