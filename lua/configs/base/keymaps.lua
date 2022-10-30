b0d7311a927ac8b992e9c3729b31a1d33897c28b
Biser Stoilov <lvim@motion-agency.com>
2022-10-30 16:50:26 +0200

    Lsp keymaps

--- lua/configs/base/keymaps.lua
+++ lua/configs/base/keymaps.lua
@@ -19,29 +19,14 @@ keymaps["normal"] = {
     { "<C-Right>", ":vertical resize +2<CR>" }, -- Resize width +
     { "<C-Up>", ":resize -2<CR>" }, -- Resize height -
     { "<C-Down>", ":resize +2<CR>" }, -- Resize height +
-    { "gd", ":LspDefinition<CR>" }, -- Lsp definition
-    { "gt", ":LspTypeDefinition<CR>" }, -- Lsp type definition
-    { "gr", ":LspReferences<CR>" }, -- Lsp references
-    { "gi", ":LspImplementation<CR>" }, -- Lsp implementation
-    { "gE", ":LspRename" }, -- Lsp rename
-    { "gf", ":LspFormatting<CR>" }, -- Lsp format code
-    { "ga", ":LspCodeAction<CR>" }, -- Lsp code action
-    { "gs", ":LspSignatureHelp<CR>" }, -- Lsp signsture help
-    { "gL", ":LspCodeLensRefresh<CR>" }, -- Lsp code lens refresh
-    { "gl", ":LspCodeLensRun<CR>" }, -- Lsp code lens run
     { "gpd", ":LspPreviewDefinition<CR>" }, -- Lsp definition
     { "gpt", ":LspPreviewTypeDefinition<CR>" }, -- Lsp type definition
     { "gpr", ":LspPreviewReferences<CR>" }, -- Lsp references
     { "gpi", ":LspPreviewImplementation<CR>" }, -- Lsp implementation
     { "gpp", ":LspPreviewCloseAll<CR>" }, -- Lsp close all
-    { "gh", ":LspHover<CR>" }, -- Lsp hover
-    { "K", ":LspHover<CR>" }, -- Lsp hover
     { "gP", ":hardcopy<CR>" }, -- Print file
     { "tn", ":tabn<CR>" }, -- Tab next
     { "tp", ":tabp<CR>" }, -- Tab prev
-    { "dc", ":LspShowDiagnosticCurrent<CR>" }, -- Lsp show diagnostic current line
-    { "dn", ":LspShowDiagnosticNext<CR>" }, -- Lsp show diagnostic next line
-    { "dp", ":LspShowDiagnostigPrev<CR>" }, -- Lsp show diagnostic prev line
 }
 
 keymaps["visual"] = {
