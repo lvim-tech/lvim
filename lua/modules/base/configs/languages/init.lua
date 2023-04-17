93d822d0a2cb0f749b319bd9c157d5ca9b7dbafb
Biser Stoilov <lvim@motion-agency.com>
2023-04-17 09:29:33 +0300

    Vimtex pdf method zathura

--- lua/modules/base/configs/languages/init.lua
+++ lua/modules/base/configs/languages/init.lua
@@ -745,6 +745,8 @@ end
 
 config.vimtex = function()
     vim.g.vimtex_mappings_prefix = ";"
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.latex_view_general_viewer = "zathura"
     vim.g.vimtex_compiler_progname = "nvr"
     vim.g.vimtex_compiler_callback_compiling = "nvr"
     vim.g.vimtex_quickfix_open_on_warning = 0
