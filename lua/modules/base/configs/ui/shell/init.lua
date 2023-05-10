500a0ba4e8966299588a09db1e6de58651c2204a
Biser Stoilov <lvim@motion-agency.com>
2023-05-10 16:49:34 +0300

    Lvim shell add config

--- lua/modules/base/configs/ui/shell/init.lua
+++ lua/modules/base/configs/ui/shell/init.lua
@@ -1,24 +1,33 @@
 local lvim_shell = require("lvim-shell")
+local config = {
+    mappings = {
+        split = "<C-x>",
+        vsplit = "<C-v>",
+        tabedit = "<C-t>",
+        edit = "<C-e>",
+        close = "<C-q>",
+    },
+}
 
 local M = {}
 
 M.Ranger = function(dir)
     dir = dir or "."
-    lvim_shell.float("ranger --choosefiles=/tmp/lvim-shell " .. dir, "l")
+    lvim_shell.float("ranger --choosefiles=/tmp/lvim-shell " .. dir, "l", config)
 end
 
 M.Vifm = function(dir)
     dir = dir or "."
-    lvim_shell.float("vifm --choose-files /tmp/lvim-shell " .. dir, "l")
+    lvim_shell.float("vifm --choose-files /tmp/lvim-shell " .. dir, "l", config)
 end
 
 M.Lazygit = function(dir)
     dir = dir or "."
-    lvim_shell.float("lazygit -w " .. dir, "<CR>")
+    lvim_shell.float("lazygit -w " .. dir, "<CR>", config)
 end
 
 M.Lazydocker = function()
-    lvim_shell.float("lazydocker", "<CR>")
+    lvim_shell.float("lazydocker", "<CR>", config)
 end
 
 return M
