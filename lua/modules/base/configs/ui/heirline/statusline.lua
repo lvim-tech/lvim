ee252092147ca3bb681d5e91ab707dcc0a3dd79a
Biser Stoilov <lvim@motion-agency.com>
2023-05-12 13:18:58 +0300

    Heirline words counter

--- lua/modules/base/configs/ui/heirline/statusline.lua
+++ lua/modules/base/configs/ui/heirline/statusline.lua
@@ -246,9 +246,15 @@ local spell = {
 }
 local statistic = {
     provider = function()
-        local words = vim.fn.wordcount().words
-        local chars = vim.fn.wordcount().chars
-        return " " .. words .. " W | " .. chars .. " Ch"
+        if _G.LVIM_SETTINGS.mode == "v" or _G.LVIM_SETTINGS.mode == "V" then
+            local visual_word_count = vim.fn.wordcount().visual_words
+            local total_word_count = vim.fn.wordcount().words
+            return " " .. visual_word_count .. " / " .. total_word_count
+        else
+            local cursor_word_count = vim.fn.wordcount().cursor_words
+            local total_word_count = vim.fn.wordcount().words
+            return " " .. cursor_word_count .. " / " .. total_word_count
+        end
     end,
     hl = { fg = common.theme_colors.fg_05, bold = true },
 }
