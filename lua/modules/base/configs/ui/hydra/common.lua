6e9d137dbd05d7646c756caf9be7d12a8d51dd0c
Biser Stoilov <lvim@motion-agency.com>
2023-05-27 18:12:05 +0300

    Hydra common

---
+++ lua/modules/base/configs/ui/hydra/common.lua
@@ -0,0 +1,110 @@
+local Hydra = require("hydra")
+local keymap = require("hydra.keymap-util")
+
+local location_hint = [[
+                           COMMON
+
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Neotree                     _e_ │ _=_          LVIM file manager
+
+LvimDiagnostics             _d_ │ _c_    Show diagnostic current
+Show diagnostic next        _n_ │ _p_       Show diagnostic prev
+
+Symbols outline             _o_ │ _t_                   Terminal
+
+Previous hunk               _[_ │ _]_                  Next hunk
+View hunk                   _v_ │ _b_                 Blame line
+
+Lazygit                     _l_ │ _f_                Lvim forgit
+
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+                         exit _<Esc>_
+]]
+
+Hydra({
+    name = "COMMON",
+    hint = location_hint,
+    config = {
+        color = "pink",
+        invoke_on_body = true,
+        hint = {
+            position = "bottom-center",
+            border = "single",
+        },
+    },
+    mode = { "n", "x", "v" },
+    body = "<leader>a",
+    heads = {
+        {
+            "e",
+            keymap.cmd("Neotree left"),
+            { silent = true, desc = "Neotree" },
+        },
+        {
+            "=",
+            keymap.cmd("LvimFileManager"),
+            { silent = true, desc = "LVIM file manager" },
+        },
+        {
+            "d",
+            keymap.cmd("LvimDiagnostics"),
+            { silent = true, desc = "Prev" },
+        },
+        {
+            "c",
+            keymap.cmd("LspShowDiagnosticCurrent"),
+            { silent = true, desc = "Show diagnostic current" },
+        },
+        {
+            "n",
+            keymap.cmd("LspShowDiagnosticNext"),
+            { silent = true, desc = "Show diagnostic next" },
+        },
+        {
+            "p",
+            keymap.cmd("LspShowDiagnosticPrev"),
+            { silent = true, desc = "Show diagnostic prev" },
+        },
+        {
+            "o",
+            keymap.cmd("SymbolsOutline"),
+            { silent = true, desc = "Symbols outline" },
+        },
+        {
+            "t",
+            keymap.cmd("TerminalHorizontal1"),
+            { silent = true, desc = "Terminal" },
+        },
+        {
+            "[",
+            keymap.cmd("GitSignsPreviewHunk"),
+            { silent = true, desc = "Previous hunk" },
+        },
+        {
+            "]",
+            keymap.cmd("GitSignsNextHunk"),
+            { silent = true, desc = "Next hunk" },
+        },
+        {
+            "v",
+            keymap.cmd("GitSignsPreviewHunk"),
+            { silent = true, desc = "View hunk" },
+        },
+        {
+            "b",
+            keymap.cmd("GitSignsBlameLine"),
+            { silent = true, desc = "Blame line" },
+        },
+        {
+            "l",
+            keymap.cmd("Lazygit"),
+            { silent = true, desc = "Lazygit" },
+        },
+        {
+            "f",
+            keymap.cmd("LvimForgit"),
+            { silent = true, desc = "LvimForgit" },
+        },
+        { "<Esc>", nil, { exit = true, desc = false } },
+    },
+})
