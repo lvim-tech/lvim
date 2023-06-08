b580832536d12b9da888bab30b117b8eda788c30
LVIM ORG <lvim@motion-agency.com>
2023-06-08 08:03:55 +0300

    Fix config treesitter_textobject

--- lua/modules/base/configs/editor/init.lua
+++ lua/modules/base/configs/editor/init.lua
@@ -573,27 +573,25 @@ end
 
 config.nvim_treesitter_textobject = function()
     require("nvim-treesitter.configs").setup({
-        require("nvim-treesitter.configs").setup({
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                    },
-                    selection_modes = {
-                        ["@parameter.outer"] = "v",
-                        ["@function.outer"] = "V",
-                        ["@class.outer"] = "<c-v>",
-                    },
-                    include_surrounding_whitespace = true,
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                    ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
                 },
+                selection_modes = {
+                    ["@parameter.outer"] = "v",
+                    ["@function.outer"] = "V",
+                    ["@class.outer"] = "<c-v>",
+                },
+                include_surrounding_whitespace = true,
             },
-        }),
+        },
     })
 end
 
