90f6eaa8ef79e92412fec29ac3310b86d4994aa6
LVIM ORG <lvim@motion-agency.com>
2023-06-11 09:48:51 +0300

    Remove comments

--- lua/modules/base/configs/languages/init.lua
+++ lua/modules/base/configs/languages/init.lua
@@ -686,10 +686,10 @@ config.nvim_dap_vscode_js = function()
         return
     end
     dap_vscode_js.setup({
-        node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-        debugger_path = global.mason_path .. "/bin/vscode-js-debug", -- Path to vscode-js-debug installation.
-        debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+        node_path = "node",
+        debugger_path = global.mason_path .. "/bin/vscode-js-debug",
+        debugger_cmd = { "js-debug-adapter" },
+        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
     })
 end
 
