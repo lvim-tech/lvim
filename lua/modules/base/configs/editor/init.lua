74e742660953faee9e07d33c378aa6e748782a7b
LVIM ORG <lvim@motion-agency.com>
2023-06-25 13:48:02 +0300

    Flash "f", "t", "F", "T" optimization

--- lua/modules/base/configs/editor/init.lua
+++ lua/modules/base/configs/editor/init.lua
@@ -785,6 +785,19 @@ config.flash_nvim = function()
     vim.keymap.set({ "n", "x", "o" }, "<C-c>,", function()
         flash.treesitter()
     end)
+    local Config = require("flash.config")
+    local Char = require("flash.plugins.char")
+    for _, motion in ipairs({ "f", "t", "F", "T" }) do
+        vim.keymap.set({ "n", "x", "o" }, motion, function()
+            require("flash").jump(Config.get({
+                mode = "char",
+                search = {
+                    mode = Char.mode(motion),
+                    max_length = 1,
+                },
+            }, Char.motions[motion]))
+        end)
+    end
 end
 
 config.todo_comments_nvim = function()
