1cc42b2a82be8d3fa8b2ac78ff3d332bafe22a05
LVIM ORG <lvim@motion-agency.com>
2023-09-30 19:05:06 +0300

    Rgflow keys fix

--- lua/modules/base/configs/editor/init.lua
+++ lua/modules/base/configs/editor/init.lua
@@ -190,7 +190,7 @@ config.rgflow_nvim = function()
     end
     rgflow.setup({
         cmd_flags = "--smart-case --fixed-strings --ignore --max-columns 200",
-        default_trigger_mappings = true,
+        default_trigger_mappings = false,
         default_ui_mappings = true,
         default_quickfix_mappings = true,
         ui_top_line_char = "",
