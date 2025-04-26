f7b8090627987cac1a33c725e52d80bb8dc44b9c
LVIM ORG <lvim@motion-agency.com>
2025-04-26 22:54:20 +0300

    Globals

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,14 +5,10 @@ if global == "unsuported" then
 else
     local funcs = require("core.funcs")
     _G.LVIM_SNAPSHOT = funcs.get_snapshot()
-    local vim = vim
     vim.g.mapleader = " "
     vim.g.maplocalleader = " "
     vim.keymap.set("n", " ", "", { noremap = true })
     vim.keymap.set("x", " ", "", { noremap = true })
-    global["diagnostics"] = {}
-    global["diagnostics"]["path"] = vim.fn.getcwd()
-    global["diagnostics"]["method"] = "global"
     _G.LVIM_SETTINGS = funcs.read_file(global.lvim_path .. "/.configs/lvim/config.json")
     local lazy = require("core.lazy")
     lazy.is_lazy()
