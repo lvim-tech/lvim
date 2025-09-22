024b53f572566e3c6584d01d9d406ed654ea04ed
LVIM ORG <lvim@motion-agency.com>
2025-09-22 08:07:56 +0300

    Lvim version support

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 _G.LVIM_NVIM_VERSION = vim.version()
-if _G.LVIM_NVIM_VERSION.major == 0 and _G.LVIM_NVIM_VERSION.minor < 11 then
-    print("LVIM IDE required Neovim >= 0.12.0")
-else
+if
+    (_G.LVIM_NVIM_VERSION.major == 0 and _G.LVIM_NVIM_VERSION.minor == 11 and _G.LVIM_NVIM_VERSION.patch >= 4)
+    or (_G.LVIM_NVIM_VERSION.major == 0 and _G.LVIM_NVIM_VERSION.minor == 12)
+then
     require("core")
+else
+    print("LVIM IDE required Neovim >= 0.11.4")
 end
