811b1b4a29198d98432efb7f1cb495e62840fbff
lvim-tech <biserstoilov@gmail.com>
2025-10-12 12:06:28 +0300

    Fix ui

--- lua/modules/base/configs/dependencies/init.lua
+++ lua/modules/base/configs/dependencies/init.lua
@@ -124,6 +124,12 @@ config.nui_nvim = function()
         end
     end
     local function override_ui_select()
+        local api = vim.api
+        local cmd = vim.cmd
+        local function set_cursor_blend(blend)
+            blend = tonumber(blend) or 0
+            cmd("hi Cursor blend=" .. blend)
+        end
         local UISelect = Menu:extend("UISelect")
         function UISelect:init(items, opts, on_done)
             local border_top_text = get_prompt_text(opts.prompt, "Select Item")
@@ -139,30 +145,24 @@ config.nui_nvim = function()
                     highlight = "FloatBorder:LvimSelectBorder",
                     style = { " ", " ", " ", " ", " ", " ", " ", " " },
                     text = {
-                        top = Text(border_top_text, "LvimSelectBorder"),
+                        top = Text(border_top_text, "LvimSelectTitle"),
                         top_align = "center",
                     },
                 },
                 win_options = {
-                    winhighlight = "Normal:LvimSelectNormal",
+                    winhighlight = "Normal:LvimSelectNormal,Title:LvimSelectTitle",
                 },
                 zindex = 999,
             }
             if kind == "codeaction" then
                 popup_options.relative = "cursor"
-                popup_options.position = {
-                    row = 2,
-                    col = 1,
-                }
+                popup_options.position = { row = 2, col = 1 }
             end
-            local max_width = popup_options.relative == "editor" and vim.o.columns - 4
-                or vim.api.nvim_win_get_width(0) - 4
+            local max_width = popup_options.relative == "editor" and vim.o.columns - 4 or api.nvim_win_get_width(0) - 4
             local max_height = popup_options.relative == "editor" and math.floor(vim.o.lines * 80 / 100)
-                or vim.api.nvim_win_get_height(0)
+                or api.nvim_win_get_height(0)
             local menu_items = {
-                UISelect.separator("", {
-                    char = " ",
-                }),
+                UISelect.separator("", { char = " " }),
             }
             for index, item in ipairs(items) do
                 if type(item) ~= "table" then
@@ -170,10 +170,10 @@ config.nui_nvim = function()
                 end
                 item.index = index
                 local item_text = string.sub(format_item(item), 0, max_width)
-                table.insert(menu_items, Menu.item(item_text, item))
+                table.insert(menu_items, Menu.item(item_text, item, { hl_group = "LvimSelectNormal" }))
             end
             local menu_options = {
-                min_width = vim.api.nvim_strwidth(border_top_text),
+                min_width = api.nvim_strwidth(border_top_text),
                 max_width = max_width,
                 max_height = max_height,
                 lines = menu_items,
@@ -187,11 +187,10 @@ config.nui_nvim = function()
             UISelect.super.init(self, popup_options, menu_options)
             self:on(event.BufLeave, function()
                 on_done(nil, nil)
+                set_cursor_blend(0)
             end, { once = true })
         end
-
         local select_ui = nil
-        ---@diagnostic disable-next-line: duplicate-set-field
         vim.ui.select = function(items, opts, on_choice)
             assert(type(on_choice) == "function", "missing on_choice function")
             if select_ui then
@@ -206,6 +205,7 @@ config.nui_nvim = function()
                 select_ui = nil
             end)
             select_ui:mount()
+            set_cursor_blend(100)
         end
     end
     override_ui_input()
